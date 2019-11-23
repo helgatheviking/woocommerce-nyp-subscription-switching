ffb154869375aee9e2550ec1ffa00c176f72f8b0
Kathy Daring <helgatheviking@gmail.com>
2019-11-23 10:34:11 -0700

    Delete functions we no longer will be using. Alert, this is NOT backcompatible-friendly.

--- woocommerce-nyp-subscription-switching.php
+++ woocommerce-nyp-subscription-switching.php
@@ -111,11 +111,6 @@ class WC_NYP_Subs_Switching {
 		// allow subscription prices to be switched
 		add_filter( 'wcs_is_product_switchable', array( $this, 'is_switchable' ), 10, 3 );
 		add_filter( 'woocommerce_subscriptions_add_switch_query_args', array( $this, 'add_switch_query_args' ), 10, 3 );
-		add_filter( 'woocommerce_dropdown_variation_attribute_options_html', array( $this, 'disable_attributes' ) );
-		add_filter( 'woocommerce_subscriptions_switch_error_message', array( $this, 'subscription_switch_validation' ), 10, 6 );
-
-		// allow nyp sub variations to be price switched
-		add_filter( 'template_redirect', array( $this, 'nyp_subscription_switch_handler' ) );
 
     }
 
@@ -231,117 +226,6 @@ class WC_NYP_Subs_Switching {
 		return $permalink;
 	}
 
-
-	/*
-	 * Disable the attribute select if switching is not allowed
-	 *
-	 * @param str $html
-	 * @return str
-	 * @since 0.1.0
-	 */
-	public function disable_attributes( $html ){
-		global $product;
-
-		if ( isset( $_GET['switch-subscription'] ) && isset( $_GET['nyp-only'] ) ){
-			$html = str_replace( '<select', '<select disabled="disabled"', $html );
-			add_filter( 'woocommerce_reset_variations_link', '__return_null' );
-		}
-
-		return $html;
-	}
-
-
-	/*
-	 * Test if the switching subscription is valid
-	 * if already valid (ie: changing variation), then skip
-	 * if not already valid, check that price or period is changed
-	 *
-	 * @param bool $is_product_switchable
-	 * @param obj $product
-	 * @return bool
-	 * @since 0.1.0
-	 */
-	public function subscription_switch_validation( $error_message, $product_id, $quantity, $variation_id, $subscription, $item ){
-
-		$nyp_id = $variation_id ? $variation_id : $product_id;
-
-		// check that $item isn't empty so that you don't accidentally override the "can't find old sub" error
-		if( ! empty( $item ) && WC_Name_Your_Price_Helpers::is_nyp( $nyp_id ) ){
-
-			$prefix = apply_filters( 'nyp_field_prefix', '', $nyp_id );
-
-			$nyp_product = wc_get_product( $nyp_id );
-
-			$initial_subscription_price = $subscription->get_line_subtotal( $item );
-			$new_subscription_price = floatval( WC_Name_Your_Price_Helpers::get_posted_price( $nyp_id, $prefix ) );
-			$initial_subscription_period = WC_Name_Your_Price_Core_Compatibility::get_prop( $subscription, 'billing_period' );
-			$new_subscription_period = WC_Name_Your_Price_Helpers::get_posted_period( $nyp_id, $prefix );
-
-			$nyp_error_message = '';
-
-			// if variable billing period check both price and billing period
-			if( WC_Name_Your_Price_Helpers::is_billing_period_variable( $nyp_id ) && $new_subscription_price == $initial_subscription_price && $new_subscription_period == $initial_subscription_period ){
-				$nyp_error_message = __( 'Please modify the price or billing period so that it is not the same as your existing subscription.', 'wc_name_your_price' );
-			} else if ( $new_subscription_price == $initial_subscription_price ){
-				$nyp_error_message = __( 'Please modify the price so that it is not the same as your existing subscription.', 'wc_name_your_price' );
-			}
-
-			// replace the error message
-			$error_message = $nyp_error_message;
-
-		}
-
-		return $error_message;
-	}
-
-	/*
-	 * Enable switching for NYP variations
-	 *
-	 * @param bool $is_product_switchable
-	 * @param obj $product
-	 * @return bool
-	 * @since 0.1.0
-	 */
-	public function nyp_subscription_switch_handler(){
-		global $post;
-		if ( ! isset( $_GET['switch-subscription'] ) && is_product() && $product = wc_get_product( $post ) ) {
-
-			// variable product with no variation switching allowed
-			if ( $product->is_type( 'variable-subscription' ) && in_array( get_option( WC_Subscriptions_Admin::$option_prefix . '_allow_switching', 'no' ), array( 'no', 'grouped' ) ) && 'no' != $product->limit_subscriptions ) {
-
-				// Check if the user has an active subscription for this product, and if so, initiate the switch process
-				$subscriptions = wcs_get_users_subscriptions();
-
-				foreach ( $subscriptions as $subscription ) {
-
-					// If already subscribed to the variation and variation is NYP
-					if ( $subscription->has_product( $product->id ) && ( 'any' == $product->limit_subscriptions || $subscription->has_status( $product->limit_subscriptions ) || ( 'active' == $product->limit_subscriptions && $subscription->has_status( 'on-hold' ) ) ) ) {
-
-						// If switching is enabled for this product type, initiate the auto-switch process
-						// Get the matching item
-						foreach ( $subscription->get_items() as $line_item_id => $line_item ) {
-							if ( $line_item['product_id'] == $product->id || $line_item['variation_id'] == $product->id ) {
-								$item_id = $line_item_id;
-								$item    = $line_item;
-								break;
-							}
-						}
-
-						if ( isset( $line_item['variation_id'] ) && WC_Name_Your_Price_Helpers::is_nyp(  $line_item['variation_id'] ) ) {
-							wp_redirect( add_query_arg( array( 'auto-switch' => 'true', 'nyp-only' => 1 ), WC_Subscriptions_Switcher::get_switch_url( $item_id, $item, $subscription ) ) );
-							exit;
-
-						}
-
-					}
-
-				}
-
-			}
-
-		}
-
-	}
 
 } //end class: do not remove or there will be no more guacamole for you
 
